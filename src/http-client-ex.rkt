85767764ff62ef8b5f9992e1b0bf4fe767745ad3
Feihong Hsu <feihong.hsu@gmail.com>
2018-11-11 09:20:07 -0600

    Only print hanzi characters

--- src/http-client-ex.rkt
+++ src/http-client-ex.rkt
@@ -5,9 +5,9 @@
 
 (define (download-text)
   (define-values (port headers)
-  (~> "http://voachinese.com"
-      string->url
-      (get-pure-port/headers #:redirections 5 #:status? #t)))
+    (~> "http://voachinese.com"
+        string->url
+        (get-pure-port/headers #:redirections 5 #:status? #t)))
 
   (printf "Headers: ~a\n" (string-replace headers "\r\n" "\n"))
 
@@ -16,15 +16,23 @@
   text)
 
 (define (get-text)
-  (define filename "voachinese.html")
+  (let ([filename "voachinese.html"])
+    (if (file-exists? filename)
+      (file->string filename)
+      (begin
+        (let ([text (download-text)])
+          (call-with-output-file filename #:exists 'truncate
+            (lambda (out)
+              (display text out)))
+          text)))))
 
-  (if (file-exists? filename)
-    (file->string filename)
-    (begin
-      (let ([text (download-text)])
-        (call-with-output-file filename #:exists 'truncate
-          (lambda (out)
-            (display text out)))
-        text))))
+(define text (get-text))
 
-(get-text)
+(define (is-hanzi? c)
+  (let ([ord (char->integer c)])
+    (and (>= ord #x4e00) (<= ord #x9fff))))
+
+
+(for ([ch text]
+      #:when (is-hanzi? ch))
+  (display ch))
