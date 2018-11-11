0bef2aa1e3c7e88c483474516d66d6b752a746a6
Feihong Hsu <feihong.hsu@gmail.com>
2018-11-11 08:50:34 -0600

    Cache result of http request

--- src/http-client-ex.rkt
+++ src/http-client-ex.rkt
@@ -15,4 +15,16 @@
   (printf "Response size: ~a\n" (string-length text))
   text)
 
-(download-text)
+(define (get-text)
+  (define filename "voachinese.html")
+
+  (if (file-exists? filename)
+    (file->string filename)
+    (begin
+      (let ([text (download-text)])
+        (call-with-output-file filename #:exists 'truncate
+          (lambda (out)
+            (display text out)))
+        text))))
+
+(get-text)
